2f89007734b30c299aeb3b3e135fa7099052ef28
chandan sharma <chandan.sharma@orahi.com>
2025-03-27 10:48:47 +0530

    add password validation

--- user_task/user/helpers.py
+++ user_task/user/helpers.py
@@ -4,3 +4,8 @@ import random, string
 def generate_salt_key(length=10):
     characters = string.ascii_uppercase + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
+
+def password_verify(password):
+    if len(password) > 8 and len(password) < 16 and any(char.isdigit() for char in password) and any(char.isupper() for char in password):
+        return True
+    return False
--- user_task/user/services.py
+++ user_task/user/services.py
@@ -2,7 +2,7 @@ from user.models import tbl_user
 from serializers import UserSerializer
 import hashlib
 import hmac
-from .helpers import generate_salt_key
+from .helpers import generate_salt_key, password_verify
 import datetime
 from jwt_utils import jwt_instance
 from exceptions import ParameterError
@@ -38,6 +38,9 @@ def register_user(payload):
         is_user_exist = tbl_user.objects.filter(email=payload['email']).exists()
         if is_user_exist:
             return {'message': 'User already exists'}, 400
+        
+        if not password_verify(payload.get('password')):
+            raise ParameterError('Invalid password format')
         
         salt_key = generate_salt_key()
         hashed_password = hmac.new(salt_key.encode(), payload.get('password').encode(), hashlib.sha512).hexdigest()
